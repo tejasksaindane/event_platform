74a6cfadefaa1627c5253c81094f5502a3be5b71
tejasksaindane <tejassaindane99@gmail.com>
2024-01-24 23:21:27 +0530

    database timeout changed

--- lib/mongodb/database/index.ts
+++ lib/mongodb/database/index.ts
@@ -12,7 +12,8 @@ export const connectToDatabase = async () => {
     cached.promise = cached.promise || mongoose.connect(
         MONGODB_URI, {
             dbName: 'evently',
-            bufferCommands:false, 
+            bufferCommands: false, 
+            connectTimeoutMS:60000
         }
     )
 
